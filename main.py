4c655248e2645be00cc9201f2e8ba78c394f945e
Saran <50368466+saran-23@users.noreply.github.com>
2021-12-20 21:39:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,75 @@
+--------------------------------------edited starts here-----------------------------
+import cv2
+import numpy as np
+from time import sleep
+
+min_width=80 #Minimum rectangle width
+height_min=80 #Minimum height of the rectangle
+
+offset=6 #Error allowed between pixel
+
+line_pos=550 #Position of the counting line
+
+delay= 60 #FPS of the video
+
+detect = []
+cars = 0
+
+
+def catch_center(x, y, w, h):
+    x1 = int(w / 2)
+    y1 = int(h / 2)
+    cx = x + x1
+    cy = y + y1
+    return cx, cy
+
+cap = cv2.VideoCapture('video.mp4')
+subtraction = cv2.bgsegm.createBackgroundSubtractorMOG()
+
+while True:
+    ret , frame1 = cap.read()
+    time = float(1/delay)
+    sleep(time)
+    grey = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(grey,(3,3),5)
+    img_sub = subtraction.apply(blur)
+    dilat = cv2.dilate(img_sub,np.ones((5,5)))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    dilated = cv2.morphologyEx (dilat, cv2. MORPH_CLOSE , kernel)
+    dilated = cv2.morphologyEx (dilated, cv2. MORPH_CLOSE , kernel)
+    contour,h=cv2.findContours(dilated,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    
+    cv2.line(frame1, (25, line_pos), (1200, line_pos), (255.127,0), 3)
+    for(i,c) in enumerate(outline):
+        (x,y,w,h) = cv2.boundingRect(c)
+        validate_contour = (w >= min_width) and (h >= min_height)
+        if not validate_outline:
+            continues
+
+        cv2.rectangle(frame1,(x,y),(x+w,y+h),(0,255,0),2)
+        center = handle_center(x, y, w, h)
+        detect.append(center)
+        cv2.circle(frame1, center, 4, (0, 0.255), -1)
+
+        for (x,y) in detect:
+            if y<(line_pos+offset) and y>(line_pos-offset):
+                cars+=1
+                cv2.line(frame1, (25, line_pos), (1200, line_pos), (0.127,255), 3)
+                detect.remove((x,y))
+                print("car is detected : "+str(cars))
+    cv2.putText(frame1, "VEHICLE COUNT : "+str(cars), (450, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255,5))
+    cv2.imshow("Original Video", frame1)
+    cv2.imshow("Detect",dilated)
+
+    if cv2.waitKey(1) == 27:
+        break
+    
+cv2.destroyAllWindows()
+cap.release()
+
+---------------------------------------edited ends here------------------------------
+
+
 import cv2
 import numpy as np
 from time import sleep
